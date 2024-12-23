d68fbac8f36fb907ac567370d99bebf78acac487
Deyan Ardi <riyan.clsg11@gmail.com>
2024-12-23 12:20:31 +0800

    feat: fix counter

--- src/components/home/Counter.svelte
+++ src/components/home/Counter.svelte
@@ -4,24 +4,21 @@
 			<div class="row row-gap-32">
 				<div class="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 flex-columns-center">
 					<div class="count-block flex-item-center">
-						<div class="counter heading3">100</div>
-						<span class="heading3">k</span>
+						<div class="counter heading3">83</div>
 					</div>
-					<div class="body1 text-secondary">Business Setup Growth</div>
+					<div class="body1 text-secondary">Rekening Tabungan</div>
 				</div>
 				<div class="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 flex-columns-center">
 					<div class="count-block flex-item-center">
-						<div class="counter heading3">10000</div>
-						<span class="heading3">k</span>
+						<div class="counter heading3">60</div>
 					</div>
-					<div class="body1 text-secondary">Business Problem Solving</div>
+					<div class="body1 text-secondary">Rekening Deposito</div>
 				</div>
 				<div class="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 flex-columns-center">
 					<div class="count-block flex-item-center">
-						<div class="counter heading3">298</div>
-						<span class="heading3">k</span>
+						<div class="counter heading3">74</div>
 					</div>
-					<div class="body1 text-secondary">Goal achiever</div>
+					<div class="body1 text-secondary">Rekening Kredit</div>
 				</div>
 				<div class="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 flex-columns-center">
 					<div class="count-block flex-item-center">
