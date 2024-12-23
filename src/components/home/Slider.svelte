b2ecde17230b7e875742cadf1d22b5cedd99a337
Deyan Ardi <riyan.clsg11@gmail.com>
2024-12-23 12:48:39 +0800

    fix: fix slider

--- src/components/home/Slider.svelte
+++ src/components/home/Slider.svelte
@@ -1,5 +1,18 @@
 <div class="partner-five-col style-one mt-100 bg-blue">
-	<div class="list pt-32 pb-32">
+	<div class="cta-three">
+		<div class="container">
+			<div class="row flex-columns-center">
+				<div class="col-lg-7 col-md-8 col-11 text-center text-white pt-80">
+					<div class="heading4">PT BPR Suryajaya Kubutambahan</div>
+					<p>
+						Berizin dan di awasi oleh Otoritas Jasa Keuangan ( OJK ) serta merupakan Bank peserta
+						penjaminan Lembaga Penjamin Simpanan (LPS)
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="list pb-32">
 		<div class="bg-img flex-center" style="width: 170px !important;">
 			<img class="w-100" src="assets/images/partner/ayo-ke-bank.png" alt="" />
 		</div>
