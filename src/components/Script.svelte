9682f3c27cb01d5940f4716e07c55a2b99efda6e
Deyan Ardi <riyan.clsg11@gmail.com>
2024-12-26 15:13:21 +0800

    feat: fix main js

--- src/components/Script.svelte
+++ src/components/Script.svelte
@@ -1,6 +1,7 @@
 <svelte:head>
-	<script src="/assets/js/jquery-3.7.0.js" defer></script>
-	<script src="/assets/js/slick.min.js" defer></script>
+	<script src="https://code.jquery.com/jquery-3.7.0.min.js" defer></script>
+	<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" defer></script>
+
 	<script src="/assets/js/waypoints.min.js" defer></script>
 	<script src="/assets/js/countUp.min.js" defer></script>
 	<script src="/assets/js/phosphor-icon.js" defer></script>
--- src/components/home/Simulasi.svelte
+++ src/components/home/Simulasi.svelte
@@ -21,148 +21,19 @@
 					</div>
 					<div class="description item-filter" data-name="simulasi-kpr">
 						<div class="title body3 text-secondary mt-16">
-							Tertarik mengajukan KPR? Kunjungi cabang terdekat kami.
+							Our unwavering vision is to be the most trusted and preferred partner in achieving
+							financial success, diligently guiding our valued clients toward a secure and
+							prosperous future.
 						</div>
-						<div class="form-simulasi">
-							<div class="container">
-								<div class="row flex-between row-gap-20">
-									<div class="col-12 p-0">
-										<div class="form-block bora-16 bg-white pt-32 flex-columns-between gap-20">
-											<div class="row row-gap-20">
-												<div class="col-12">
-													<label for="jumlah-pinjaman">Jumlah Pinjaman</label>
-													<input
-														class="w-100 bg-surface caption1 pl-16 pr-16 pt-12 pb-12 bora-8"
-														type="number"
-														min="0"
-														step="100000"
-														id="jumlah-pinjaman"
-														placeholder="0"
-													/>
-												</div>
-												<div class="col-12 col-md-8">
-													<label for="jangka-waktu">Jangka Waktu</label>
-													<select
-														id="jangka-waktu"
-														class="w-100 bg-surface caption1 pl-12 pt-12 pb-12 bora-8"
-														name="jangka_waktu"
-													>
-														<option value="1" selected>1 Tahun</option>
-														<option value="2">2 Tahun</option>
-														<option value="3">3 Tahun</option>
-														<option value="4">4 Tahun</option>
-														<option value="5">5 Tahun</option>
-														<option value="6">6 Tahun</option>
-														<option value="7">7 Tahun</option>
-														<option value="8">8 Tahun</option>
-														<option value="9">9 Tahun</option>
-														<option value="10">10 Tahun</option>
-														<option value="11">11 Tahun</option>
-														<option value="12">12 Tahun</option>
-														<option value="13">13 Tahun</option>
-														<option value="14">14 Tahun</option>
-														<option value="15">15 Tahun</option>
-														<option value="16">16 Tahun</option>
-														<option value="17">17 Tahun</option>
-														<option value="18">18 Tahun</option>
-														<option value="19">19 Tahun</option>
-														<option value="20">20 Tahun</option>
-													</select><i class="ph ph-caret-down"></i>
-												</div>
-												<div class="col-12 col-md-4">
-													<label for="suku-bunga">Suku Bunga</label>
-													<input
-														class="w-100 bg-surface caption1 pl-16 pr-16 pt-12 pb-12 bora-8"
-														type="text"
-														value="5%"
-														disabled
-													/>
-												</div>
-											</div>
-											<div class="button-block">
-												<button
-													class="button-share hover-bg-gradient bg-on-surface text-white text-button pl-36 pr-36 pt-12 pb-12 bora-48"
-													>Hitung</button
-												>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						<div class="more-infor mt-24"></div>
 					</div>
 					<div class="description item-filter hide" data-name="simulasi-kprs">
 						<div class="title body3 text-secondary mt-16">
-							Our mission is to provide comprehensive and personalized financial solutions that
-							empower our clients to achieve their financial goals and secure their future.
+							Our unwavering vision is to be the most trusted and preferred partner in achieving
+							financial success, diligently guiding our valued clients toward a secure and
+							prosperous future.
 						</div>
-						<div class="form-simulasi">
-							<div class="container">
-								<div class="row flex-between row-gap-20">
-									<div class="col-12 p-0">
-										<div class="form-block bora-16 bg-white pt-32 flex-columns-between gap-20">
-											<div class="row row-gap-20">
-												<div class="col-12">
-													<label for="jumlah-pinjaman">Jumlah Pinjaman</label>
-													<input
-														class="w-100 bg-surface caption1 pl-16 pr-16 pt-12 pb-12 bora-8"
-														type="number"
-														min="0"
-														step="100000"
-														id="jumlah-pinjaman"
-														placeholder="0"
-													/>
-												</div>
-												<div class="col-12 col-md-8">
-													<label for="jangka-waktu">Jangka Waktu</label>
-													<select
-														id="jangka-waktu"
-														class="w-100 bg-surface caption1 pl-12 pt-12 pb-12 bora-8"
-														name="jangka_waktu"
-													>
-														<option value="1" selected>1 Tahun</option>
-														<option value="2">2 Tahun</option>
-														<option value="3">3 Tahun</option>
-														<option value="4">4 Tahun</option>
-														<option value="5">5 Tahun</option>
-														<option value="6">6 Tahun</option>
-														<option value="7">7 Tahun</option>
-														<option value="8">8 Tahun</option>
-														<option value="9">9 Tahun</option>
-														<option value="10">10 Tahun</option>
-														<option value="11">11 Tahun</option>
-														<option value="12">12 Tahun</option>
-														<option value="13">13 Tahun</option>
-														<option value="14">14 Tahun</option>
-														<option value="15">15 Tahun</option>
-														<option value="16">16 Tahun</option>
-														<option value="17">17 Tahun</option>
-														<option value="18">18 Tahun</option>
-														<option value="19">19 Tahun</option>
-														<option value="20">20 Tahun</option>
-													</select><i class="ph ph-caret-down"></i>
-												</div>
-												<div class="col-12 col-md-4">
-													<label for="suku-bunga">Suku Bunga</label>
-													<input
-														class="w-100 bg-surface caption1 pl-16 pr-16 pt-12 pb-12 bora-8"
-														type="text"
-														value="5%"
-														disabled
-													/>
-												</div>
-											</div>
-											<div class="button-block">
-												<button
-													class="button-share hover-bg-gradient bg-on-surface text-white text-button pl-36 pr-36 pt-12 pb-12 bora-48"
-													>Hitung</button
-												>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+						<div class="more-infor mt-24"></div>
 					</div>
 					<div class="description item-filter hide" data-name="simulasi-deposito">
 						<div class="title body3 text-secondary mt-16">
