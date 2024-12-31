7b3ed52cb871aa8205818b4b78ca5c3ea60c2f90
Deyan Ardi <riyan.clsg11@gmail.com>
2024-12-31 11:54:07 +0800

    feat: add 505

---
+++ src/routes/+error.svelte
@@ -0,0 +1,33 @@
+<div class="slider-sub">
+	<div class="bg-img">
+		<img loading="lazy" src="/assets/images/banner/about1.png" alt="banner" />
+	</div>
+	<div class="container">
+		<div class="heading-nav gap-4 mt-32">
+			<a class="hover-underline caption1 text-white" href="/">Beranda</a><i
+				class="ph ph-caret-double-right text-white"
+			></i>
+			<div class="caption1 text-white">Galat</div>
+		</div>
+		<div class="text-nav">
+			<div class="heading3 text-white">Galat Error</div>
+			<div class="sub-heading mt-8 text-white fw-400">
+				Halaman tidak dapat ditemukan, periksa kembali tautan Anda
+			</div>
+		</div>
+	</div>
+</div>
+<div class="blog-list style-detail pb-60">
+	<div class="container">
+		<div class="d-flex justify-content-center align-items-center">
+			<div class="col-12 col-lg-5 text-center">
+				<img
+					loading="lazy"
+					class="w-100 h-100 hover-scale display-block"
+					src="/assets/images/error.svg"
+					alt="Page Error"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
