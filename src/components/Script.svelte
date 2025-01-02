641f404647a0b1fa226e9fc17329dcf56d4b5098
Deyan Ardi <riyan.clsg11@gmail.com>
2025-01-02 13:05:04 +0800

    remove: remove icon

--- src/components/Script.svelte
+++ src/components/Script.svelte
@@ -22,7 +22,6 @@
 
 	<script src="/assets/js/waypoints.min.js" defer></script>
 	<script src="/assets/js/countUp.min.js" defer></script>
-	<script src="/assets/js/phosphor-icon.js" defer></script>
 	<script src="/assets/js/scrollreveal-4.0.0.min.js" defer></script>
 	<script src="/assets/js/bootstrap-drawer.min.js" defer></script>
 	<script src="/assets/js/drawer.min.js" defer></script>
