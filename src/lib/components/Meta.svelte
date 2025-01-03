a0db6b3d80c5039928823fb25365e6f27c35e0bf
Deyan Ardi <riyan.clsg11@gmail.com>
2025-01-03 14:43:57 +0800

    fix: fix meta

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -2,27 +2,29 @@
 	import Favicon from '$lib/images/favicon.ico';
 </script>
 
-<!-- Meta Here -->
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<meta http-equiv="x-ua-compatible" content="ie=edge" />
-<meta name="description" content="BPR Suryajaya Kubutambahan - Semakin Dekat di Hati" />
-<meta name="robots" content="index,follow" />
-<meta
-	name="keywords"
-	content="bpr, bpr suryajaya kubutambahan, suryajaya, bank, perekonomian, rakyat"
-/>
-<meta name="author" content="IT BPR Suryajaya Kubutambahan" />
-<meta property="og:title" content="BPR Suryajaya Kubutambahan" />
-<meta property="og:description" content="BPR Suryajaya Kubutambahan - Semakin Dekat di Hati" />
-<meta property="og:image" content={Favicon} />
-<meta property="og:url" content="https://bpr-suryajaya-kbt.netlify.app" />
-<meta property="og:type" content="page" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="reply-to" content="info@bprsuryajaya.com" />
-<meta name="owner" content="info@bprsuryajaya.com" />
-<meta name="copyright" content="IT BPR Suryajaya Kubutambahan" />
-<meta name="expires" content="Saturday, 17-Feb-30 08:27:02 GMT" />
-<meta name="web_author" content="IT BPR Suryajaya Kubutambahan" />
-<meta name="web_author" content="https://bpr-suryajaya-kbt.netlify.app" />
-<!-- End Meta -->
+<svelte:head>
+	<!-- Meta Here -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="description" content="BPR Suryajaya Kubutambahan - Semakin Dekat di Hati" />
+	<meta name="robots" content="index,follow" />
+	<meta
+		name="keywords"
+		content="bpr, bpr suryajaya kubutambahan, suryajaya, bank, perekonomian, rakyat"
+	/>
+	<meta name="author" content="IT BPR Suryajaya Kubutambahan" />
+	<meta property="og:title" content="BPR Suryajaya Kubutambahan" />
+	<meta property="og:description" content="BPR Suryajaya Kubutambahan - Semakin Dekat di Hati" />
+	<meta property="og:image" content={Favicon} />
+	<meta property="og:url" content="https://bpr-suryajaya-kbt.netlify.app" />
+	<meta property="og:type" content="page" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="reply-to" content="info@bprsuryajaya.com" />
+	<meta name="owner" content="info@bprsuryajaya.com" />
+	<meta name="copyright" content="IT BPR Suryajaya Kubutambahan" />
+	<meta name="expires" content="Saturday, 17-Feb-30 08:27:02 GMT" />
+	<meta name="web_author" content="IT BPR Suryajaya Kubutambahan" />
+	<meta name="web_author" content="https://bpr-suryajaya-kbt.netlify.app" />
+	<!-- End Meta -->
+</svelte:head>
